6a1369afa07620d5879d9a45d09521b2244eac79
Troels Knak-Nielsen <troelskn@gmail.com>
2016-03-15 11:19:08 +0100

    Fixed some errors in manipulation (purchase in particular) of shipments

--- lib/Sendworks/ShipmentsCollection.php
+++ lib/Sendworks/ShipmentsCollection.php
@@ -18,23 +18,15 @@ class ShipmentsCollection {
   }
 
   function fetch($mixed) {
-    if ($mixed instanceOf ShipmentRef) {
-      $path = $mixed->url;
-    } else {
-      $path = "shipments/$mixed";
-    }
+    $path = $this->buildUrl($mixed);
     $response = $this->client()->get($path);
     if ($response->getStatusCode() == 200) {
       return Shipment::import(json_decode($response->getBody(), true), $this->connection);
     }
   }
 
-  function rates($shipment) {
-    if ($mixed instanceOf ShipmentRef) {
-      $path = $mixed->url . "/rates";
-    } else {
-      $path = "shipments/$mixed/rates";
-    }
+  function rates($mixed) {
+    $path = $this->buildUrl($mixed) . "/rates";
     $response = $this->client()->get($path);
     $result = array();
     foreach (json_decode($response->getBody(), true) as $struct) {
@@ -43,27 +35,23 @@ class ShipmentsCollection {
     return $result;
   }
 
-  function buy($shipment) {
-    if ($mixed instanceOf ShipmentRef) {
-      $path = $mixed->url . "/buy";
-    } else {
-      $path = "shipments/$mixed/buy";
-    }
+  function buy($mixed) {
+    $path = $this->buildUrl($mixed) . "/buy";
     $response = $this->client()->post($path);
     if ($response->getStatusCode() == 200) {
       return Shipment::import(json_decode($response->getBody(), true), $this->connection);
+    } else {
+      $this->raiseError($response);
     }
   }
 
-  function cancel($shipment) {
-    if ($mixed instanceOf ShipmentRef) {
-      $path = $mixed->url . "/cancel";
-    } else {
-      $path = "shipments/$mixed/cancel";
-    }
+  function cancel($mixed) {
+    $path = $this->buildUrl($mixed) . "/cancel";
     $response = $this->client()->post($path);
     if ($response->getStatusCode() == 200) {
       return Shipment::import(json_decode($response->getBody(), true), $this->connection);
+    } else {
+      $this->raiseError($response);
     }
   }
 
@@ -76,11 +64,33 @@ class ShipmentsCollection {
     $response = $this->client()->post($path, ['json' => $shipment->toHash()]);
     if ($response->getStatusCode() == 200) {
       return Shipment::import(json_decode($response->getBody(), true), $this->connection);
+    } else {
+      $this->raiseError($response);
     }
   }
 
   protected function client() {
     return $this->connection->client();
+  }
+
+  protected function buildUrl($mixed) {
+    if ($mixed instanceOf ShipmentRef) {
+      return $mixed->url;
+    } else if ($mixed instanceOf Shipment) {
+      return "shipments/" . $mixed->id;
+    } else {
+      return "shipments/$mixed";
+    }
+    throw new \Exception("Unexpected input");
+  }
+
+  protected function raiseError($response) {
+    $http_code = $response->getStatusCode();
+    $json = json_decode($response->getBody(), true);
+    if (isset($json['message'])) {
+      throw new \Exception("Error ($http_code): " . $json['message']);
+    }
+    throw new \Exception("Error ($http_code)");
   }
 
 }
